b35cdff7d71121d8b1c33b83de5835fa2dadb616
rgeraldow <rgeraldow@gmail.com>
2024-09-18 07:11:35 +0700

    bismillah deploy pws

--- main/views.py
+++ main/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from main.forms import CarEntryForm
 from main.models import CarEntry
+from django.http import HttpResponse
+from django.core import serializers
 
 def show_main(request):
     car_entries = CarEntry.objects.all()
@@ -24,3 +26,18 @@ def create_car_entry(request):
     context = {'form': form}
     return render(request, "create_car_entry.html", context)
 
+def show_xml(request):
+    data = CarEntry.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    data = CarEntry.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_xml_by_id(request, id):
+    data = CarEntry.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json_by_id(request, id):
+    data = CarEntry.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
